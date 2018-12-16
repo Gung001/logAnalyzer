273472c3c1b0c48aa9787fcc67fafe48bf5b226c
Gryant <gung001@163.com>
2018-12-16 10:47:41 +0800

    storm 集成 hbase

---
+++ src/main/java/com/lxgy/storm/integration/hbase/LocalHbaseWordCount.java
@@ -0,0 +1,150 @@
+package com.lxgy.storm.integration.hbase;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.hbase.bolt.HBaseBolt;
+import org.apache.storm.hbase.bolt.mapper.SimpleHBaseMapper;
+import org.apache.storm.hdfs.bolt.HdfsBolt;
+import org.apache.storm.hdfs.bolt.format.DefaultFileNameFormat;
+import org.apache.storm.hdfs.bolt.format.DelimitedRecordFormat;
+import org.apache.storm.hdfs.bolt.format.FileNameFormat;
+import org.apache.storm.hdfs.bolt.format.RecordFormat;
+import org.apache.storm.hdfs.bolt.rotation.FileRotationPolicy;
+import org.apache.storm.hdfs.bolt.rotation.FileSizeRotationPolicy;
+import org.apache.storm.hdfs.bolt.sync.CountSyncPolicy;
+import org.apache.storm.hdfs.bolt.sync.SyncPolicy;
+import org.apache.storm.redis.common.mapper.RedisDataTypeDescription;
+import org.apache.storm.redis.common.mapper.RedisStoreMapper;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.ITuple;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @author Gryant
+ */
+public class LocalHbaseWordCount {
+
+    public static class DataSourceSpout extends BaseRichSpout {
+
+        SpoutOutputCollector collector;
+        public static final String[] words = new String[]{"apple", "orange", "pineapple", "banana", "watermelon"};
+
+        @Override
+        public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+            this.collector = collector;
+        }
+
+        @Override
+        public void nextTuple() {
+            String word = words[new Random().nextInt(words.length)];
+
+            this.collector.emit(new Values(word));
+
+            System.out.println("emit:" + word);
+
+            Utils.sleep(1000);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word"));
+        }
+
+    }
+
+    public static class CountBolt extends BaseRichBolt {
+
+        OutputCollector collector;
+        Map<String, Integer> countMap = new HashMap<>();
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+            this.collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple input) {
+            String word = input.getStringByField("word");
+            if (!countMap.containsKey(word)) {
+                countMap.put(word, 1);
+            } else {
+                countMap.put(word, countMap.get(word) + 1);
+            }
+
+            this.collector.emit(new Values(word, countMap.get(word)));
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word", "count"));
+        }
+    }
+
+    public static class WordCountStoreMapper implements RedisStoreMapper {
+        private RedisDataTypeDescription description;
+        private final String hashKey = "wordCount";
+
+        public WordCountStoreMapper() {
+            description = new RedisDataTypeDescription(
+                    RedisDataTypeDescription.RedisDataType.HASH, hashKey);
+        }
+
+        @Override
+        public RedisDataTypeDescription getDataTypeDescription() {
+            return description;
+        }
+
+        @Override
+        public String getKeyFromTuple(ITuple tuple) {
+            return tuple.getStringByField("word");
+        }
+
+        @Override
+        public String getValueFromTuple(ITuple tuple) {
+            return tuple.getIntegerByField("count") + "";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("DataSourceSpout", new DataSourceSpout());
+        builder.setBolt("CountBolt", new CountBolt()).shuffleGrouping("DataSourceSpout");
+
+        SimpleHBaseMapper mapper = new SimpleHBaseMapper()
+                .withRowKeyField("word")
+//                .withColumnFields(new Fields("word"))
+                .withCounterFields(new Fields("count"))
+                .withColumnFamily("cf");
+
+        Config config = new Config();
+        Map<String, Object> hbbConf = new HashMap<>();
+        final String HBASE_CONFIG_KEY = "hbase.conf";
+        hbbConf.put("hbase.rootdir", "hdfs://data01/hbase");
+        hbbConf.put("hbase.cluster.distributed", true);
+        hbbConf.put("hbase.zookeeper.quorum", "data01");
+        hbbConf.put("hbase.tmp.dir", "/opt/spark2.0/hbase/hbase/tmp");
+        config.put(HBASE_CONFIG_KEY, hbbConf);
+
+        HBaseBolt hBaseBolt = new HBaseBolt("word_count", mapper)
+                .withConfigKey(HBASE_CONFIG_KEY);
+
+        builder.setBolt("HBaseBolt", hBaseBolt).shuffleGrouping("CountBolt");
+
+        LocalCluster localCluster = new LocalCluster();
+        localCluster.submitTopology("LocalHDFSWordCount", config, builder.createTopology());
+    }
+}
